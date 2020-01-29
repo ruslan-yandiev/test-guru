b5f315bd3ba05703bde278a03e50c6a7fa0d32ed
ruslan <sqwer1986@gmail.com>
2020-01-29 10:23:50 +0300

    change db

--- db/migrate/20200129050816_create_tests.rb
+++ db/migrate/20200129050816_create_tests.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
       t.string :title
-      t.integer :level
+      t.integer :level, default: 1
 
       t.timestamps
     end
