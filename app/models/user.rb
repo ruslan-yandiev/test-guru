82a8ffb85cb0e23b00d9f1bfcb7b2c9170f81895
ruslan <sqwer1986@gmail.com>
2020-02-01 14:54:08 +0300

    add reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :login, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 5 }
 end
