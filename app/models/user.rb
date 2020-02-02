63b770f62571108404881e3e24b98989652c4c09
ruslan <sqwer1986@gmail.com>
2020-02-02 12:01:57 +0300

    add new method in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   validates :login, presence: true, length: { minimum: 5 }
   validates :password, presence: true, length: { minimum: 5 }
 
-  
+  def tests_list(value_level)
+  	Test.joins("JOIN user_connection_tests ON tests.id = user_connection_tests.test_id")
+  	.where(level: value_level, user_connection_tests: { user_id: id })
+  end
 end
