0462dfd3cdd566cf8d58d2cb5c52269a85ab8b5d
ruslan <sqwer1986@gmail.com>
2020-02-02 13:59:59 +0300

    add class method sort_names_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.sort_names_by_category(name_category)
+    joins('JOIN categories ON categories.id = tests.category_id')
+    .where(categories: { title: name_category })
+  end
 end
