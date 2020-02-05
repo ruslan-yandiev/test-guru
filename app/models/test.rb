ef498764c3e4cee61cb79251faf9bea1fc587784
ruslan <sqwer1986@gmail.com>
2020-02-05 07:49:36 +0300

    change models question and test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,6 @@
 class Test < ApplicationRecord
+  has_many :questions # self.has_many(:questions)
+   
   def self.sort_names_by_category(name_category)
     joins('JOIN categories ON categories.id = tests.category_id')
     .where(categories: { title: name_category })
