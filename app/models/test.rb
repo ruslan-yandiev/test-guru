d460ebcf5f11edbcce2459557f273d8737c3086c
ruslan <sqwer1986@gmail.com>
2020-02-05 13:02:43 +0300

    new change models test.rb and user.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category, optional: true
   belongs_to :author, class_name: 'User'
-  has_many :user_connection_test, dependent: :destroy
+  has_many :user_connection_tests, dependent: :destroy
   has_many :questions, dependent: :destroy
-  has_many :users, through: :user_connection_test
+  has_many :users, through: :user_connection_tests
    
   def self.sort_names_by_category(name_category)
     joins('JOIN categories ON categories.id = tests.category_id')
