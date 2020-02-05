a2fd996344d294955e90b78034200d54701a445e
ruslan <sqwer1986@gmail.com>
2020-02-05 15:22:05 +0300

    new change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+user = User.create!(login: 'cool-net', password: 'my_password')
+
 user2 = User.create!(login: 'cool-net2', password: 'my_password2')
 
 categories = Category.create!([{ title: 'Люди' }, { title: 'Животные' }])
@@ -7,8 +9,8 @@ categories = Category.create!([{ title: 'Люди' }, { title: 'Животные
 tests = Test.create!([
                { title: 'Образование', level: 1, category: categories[0], author: user2 },
                { title: 'Здоровье', level: 2, category: categories[0], author: user2 },
-               { title: 'Особенности', level: 1, category: categories[1], author: user2 },
-               { title: 'Виды', level: 2, category: categories[1], author: user2 }
+               { title: 'Особенности', level: 1, category: categories[1], author: user },
+               { title: 'Виды', level: 2, category: categories[1], author: user }
 ])
 
 questions = Question.create([
@@ -24,8 +26,6 @@ Answer.create!([
                { body: 'Брахиозавр', question: questions[0] }
 ])
 
-user = User.create!(login: 'cool-net', password: 'my_password')
-
 UserConnectionTest.create([
                { user: user, test: tests[0] },
                { user: user, test: tests[1] },
@@ -33,7 +33,7 @@ UserConnectionTest.create([
                { user: user, test: tests[3] }
 ])
 
-Test.create!( title: 'existing test', category: categories[0], author: user)
+Test.create!( title: 'existing test', category: categories[1], author: user)
 
-Test.create!(title: 'just_test', author: User.find_by(login: 'cool-net2'))
+Test.create!(title: 'does_not_have_a_category', author: User.find_by(login: 'cool-net2'))
 
