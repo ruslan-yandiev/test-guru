ebb0d9dd3a0dbdf403c2613f245f9c84248af198
ruslan <sqwer1986@gmail.com>
2020-02-09 23:19:49 +0300

    add validation in answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   scope :true_answer, -> { where(correct: true) }
+
+  validates :body, presence: true
+  validate :validate_max_limit_answers
+
+  def validate_max_limit_answers
+    errors.add(:question, 'limit is exceeded') unless question.answers.count < 4
+  end
 end
