7e39baf22ffd6f5f5d602478ad0687cc2e755c95
ruslan <sqwer1986@gmail.com>
2020-03-13 06:13:34 +0300

    add method show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,13 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: :index
+  before_action :set_test, only: %i[index]
+  before_action :set_question, only: %i[show]
 
   def index
   	render json: { questions: @test.questions }
   end
 
   def show
-  	
+  	render json: { question: @question }
   end
 
   def create
@@ -27,4 +28,7 @@ class QuestionsController < ApplicationController
   	@test = Test.find(params[:test_id])
   end
 
+  def set_question
+    @question = Question.find(params[:id])
+  end
 end
