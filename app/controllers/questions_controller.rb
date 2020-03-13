c55bbdb78e8c1e083e9914d4462d014698462d97
ruslan <sqwer1986@gmail.com>
2020-03-13 07:20:16 +0300

    add methods bew and create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_test, only: %i[index]
+  before_action :set_test, only: %i[index create delete]
   before_action :set_question, only: %i[show]
 
   def index
@@ -11,11 +11,15 @@ class QuestionsController < ApplicationController
   end
 
   def create
-  	
+    if @test.questions.create(question_params)
+      redirect_to test_questions_path
+    else
+      render :new
+    end
   end
 
   def new
-  	
+  	@question = Question.new
   end
 
   def delete
@@ -30,5 +34,9 @@ class QuestionsController < ApplicationController
 
   def set_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
   end
 end
