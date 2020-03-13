506516f9f3c9e50d89f5b368cccaaa6a86e8ac54
ruslan <sqwer1986@gmail.com>
2020-03-13 22:06:21 +0300

    start lesson8

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,8 @@ class QuestionsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
+
+
   def index
   	render json: { questions: @test.questions }
   end
