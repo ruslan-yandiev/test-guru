fe5a59f429bf804d3d8158e892fb421a6dec4500
ruslan <sqwer1986@gmail.com>
2020-03-19 18:31:51 +0300

    create views result and create in test_passage_controller methods and change model test_passage

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,4 +1,7 @@
 class TestPassagesController < ApplicationController
+
+  before_action :set_test_passage, only: %i[show update result]
+
   # метод будет получать форму для ответа на текущий вопрос
   # отступим немного от архитектуры REST 
   def show
@@ -14,6 +17,18 @@ class TestPassagesController < ApplicationController
   # чтобы проверить верно ли ответил пользователь на вопрос или нет и если верно увеличить
   # в таблице атрибут correct_questions
   def update
+    @test_passage.accept!(params[:answer_ids])
+    
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
+  end
 
+  private
+
+  def set_test_passage
+    @test_passage = TestPassage.find(params[:id])
   end
 end
