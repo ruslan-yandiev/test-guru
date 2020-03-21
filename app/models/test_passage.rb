5535cfce5e04ea14ef9bc0aa420263a83a57eeb4
ruslan <sqwer1986@gmail.com>
2020-03-21 08:23:35 +0300

    change method correct_answer? in model Testpassage

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -14,9 +14,7 @@ class TestPassage < ApplicationRecord
   end
 
   def accept!(answer_ids)
-  	if correct_answer?(answer_ids)
-  	  self.correct_questions += 1
-  	end
+  	self.correct_questions += 1 if correct_answer?(answer_ids)
 
   	save!
   end
@@ -53,7 +51,11 @@ class TestPassage < ApplicationRecord
 
   # проверим правильность ответа
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    if answer_ids.nil?
+      false
+    else
+      correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    end
   end
 
   def correct_answers
