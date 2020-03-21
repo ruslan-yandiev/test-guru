3f7ca320ce0603d04954569ac94b8b594101c100
ruslan <sqwer1986@gmail.com>
2020-03-21 08:31:56 +0300

    create helper final_result in test_passages_helper

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,3 +1,9 @@
 module TestPassagesHelper
-  
+   def final_result
+    if @test_passage.percent_result >= 85
+      'You have successfully passed the test'
+    else
+      'you failed the test'
+    end
+  end
 end
