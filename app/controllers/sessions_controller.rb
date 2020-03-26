df363be4554a72a606c3eb88fd9da4e3107a2fce
ruslan <sqwer1986@gmail.com>
2020-03-26 09:47:18 +0300

    change seed and application_controller and sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:email])
+
+  	# с помощью & построим логику, что если мы пользователя не найдем по email
+  	# то в переменной user будет значение nil и за счет & все выражение user&.authenticate(params[:password])
+  	# тоже вычеслится в nil и мы уйдем в ветку else
+  	if user&.authenticate(params[:password])
+  	  # метод для работы с пользовательскими сессиями, похож на хэш, а значит можем присваивать значение ключу
+  	  session[:user_id] = user.id
+  	  redirect_to tests_path
+  	  # с помощью метода session мы в дальнейшем сможем проверить есть ли у нас идентификатор пользователя в сессии в куки 
+  	  # и понять можем ли мы идентифицировать пользователя или нет.
+  	else
+  	  render :new
+  	end
+  end
 end
