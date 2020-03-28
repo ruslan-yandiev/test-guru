35110244707fa074bd12cb29a9e97c968c68a993
ruslan <sqwer1986@gmail.com>
2020-03-28 11:09:02 +0300

    create in application_helper.rb flash method flash_massage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, rep)
     "https://github.com/#{author}/#{rep}"
   end
+
+  def flash_massage(type)
+    content_tag(:p, flash[type], class: "flash #{type}") if flash[type] 
+  end
 end
