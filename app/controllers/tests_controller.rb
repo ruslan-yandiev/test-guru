725531e7d1261a92b60d6d80b6135a320e164d94
ruslan <sqwer1986@gmail.com>
2020-03-28 14:36:48 +0300

    change method start in tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -50,10 +50,10 @@ class TestsController < ApplicationController
   def start
     # добавим выбранному пользователю, через вызов метода ассоциации tests и использовав метод push в его
     # спец массив(коллекцию) ActiveRecord::Associations::CollectionProxy [] выбранный тест
-    @user.tests.push(@test)
+    current_user.tests.push(@test)
     # нужно создать в модели User метод test_passage чтобы получить не коллекцию, а конкретный объект класса TestPassage
     # перенапрявим на последний факт прохождения теста.
-    redirect_to @user.test_passage(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
