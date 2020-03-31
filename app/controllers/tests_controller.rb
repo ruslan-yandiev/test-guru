5e53a77f4e8fd32c203b0cc5a3b637fb2fb287f1
ruslan <sqwer1986@gmail.com>
2020-03-31 19:04:40 +0300

    create controller tests_controller.rb and base_controller in /controllers/admin

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,51 +1,12 @@
 class TestsController < ApplicationController
   # authenticate_user! также реализован в devise
   before_action :authenticate_user!
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_test, only: :start
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
     @tests = Test.all
-  end
-
-  def show; end
-
-  def new
-    # создадим экземпляр не сохраняя его в базу
-    @test = Test.new
-  end
-
-  def edit; end
-
-  def create
-    # создадим экземпляр не сохраняя его в базу и с передачей стронг параметров.
-  	@test = Test.new(test_params)
-
-    # Если объект успешно сохранен в базу то перенаправить на отображение объекта
-    if @test.save
-      redirect_to @test
-    else
-      # указываем, что мы хотим отрендерить шаблон связанный с методо new и сделать только это
-      # то есть код самого метода new выполняться не будет!!!
-      # А так как экземпляр при вызове экшена create уже создан, нам будет, что рендерить. 
-      render :new
-    end
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    # destroy удалит объект из БД, но объект в оперативной памяти останется 
-    # и мы можем обратиться к нему к примеру, чтобы отобразить какой тест удален
-    @test.destroy
-    redirect_to tests_path
   end
 
   # будет отвечать за начало прохождения теста
@@ -62,11 +23,6 @@ class TestsController < ApplicationController
 
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  # стронг параметры, для защиты. Рельсы обязуют.
-  def test_params
-  	params.require(:test).permit(:title, :level, :category_id)
   end
 
   def rescue_with_test_not_found
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
+  has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
@@ -8,10 +13,6 @@ class User < ApplicationRecord
         :trackable,
         :validatable,
         :confirmable
-
-  has_many :test_passages, dependent: :destroy
-  has_many :tests, through: :test_passages
-  has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
 
   def tests_list(value_level)
   	tests.where(level: value_level)
