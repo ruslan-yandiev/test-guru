e860113ccd46ca4b5dee27e045ac9f48af9e7528
ruslan <sqwer1986@gmail.com>
2020-04-01 13:51:13 +0300

    create method after_sign_in_path_for in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # защита от пдделки POST запросов смотреть API
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  # # https://github.com/heartcombo/devise/wiki/How-To:-Redirect-to-a-specific-page-on-successful-sign-in,-sign-up,-or-sign-out
+  def after_sign_in_path_for(user)
+    if user.is_a?(Admin)
+      admin_tests_path
+    else
+      super
+    end
+  end
 end
