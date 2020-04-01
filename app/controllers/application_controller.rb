571aa1614c7df8eb6dff3b254b9e398c4d61674b
ruslan <sqwer1986@gmail.com>
2020-04-01 17:14:41 +0300

    create method configure_permitted_parameters in ApplicationController and add validates first_name, last_name to all Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # защита от пдделки POST запросов смотреть API
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
 
+  protected
 
   # # https://github.com/heartcombo/devise/wiki/How-To:-Redirect-to-a-specific-page-on-successful-sign-in,-sign-up,-or-sign-out
   def after_sign_in_path_for(user)
@@ -13,4 +17,15 @@ class ApplicationController < ActionController::Base
       super
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
+  end
+
+  # как вариант
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password])
+  # end
 end
