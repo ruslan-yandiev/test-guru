91fb7e3347db6a2aa5e54164489169e4f5003459
ruslan <sqwer1986@gmail.com>
2020-04-03 02:15:25 +0300

    localized admin/questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,9 @@
 module QuestionsHelper
   def question_header(question)
-  	# проверим новая это запись(объект)
-  	return "Create New Question in Test: #{@test.title}" if question.new_record?
+  	# # проверим новая это запись(объект)
+  	# return "Create New Question in Test: #{@test.title}" if question.new_record?
 
-  	# проверим существует ли объект
-  	return "Edit Question in Test: #{question.test.title}" if question.persisted?
+  	# # проверим существует ли объект
+  	# return "Edit Question in Test: #{question.test.title}" if question.persisted?
   end
 end
