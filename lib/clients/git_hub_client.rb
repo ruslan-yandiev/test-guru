e548c38dd9bb80bdc73ce3f14f7af88140963c39
ruslan <sqwer1986@gmail.com>
2020-04-06 21:57:03 +0300

    generate token

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,9 +1,19 @@
 class GitHubClient
 
   ROOT_ENDPOINT = 'https://api.github.com'
+  ACCESS_TOKEN = '0c0f44b0395d1a3d0afc5f59cdafdd925d516a92'
 
   def initialize
     @http_client = setup_http_client
+  end
+
+  def create_gist(params)
+    # нужно помнить, что параметры преданные в params должны быть в json формате
+    @http_client.post('gists', params) do |request|
+      request.headers['Authorization'] = "token #{ENV['ACCESS_TOKEN']}"
+      request.headers['Content-Type'] = 'application/json'
+      request.body = params.to_json # преобразуем данные в формат json
+    end
   end
 
   private
