c63e0623fba9da4b79a36996b0bebf8449209470
ruslan <sqwer1986@gmail.com>
2020-04-09 15:08:57 +0300

    correct method link_to_question in GistsHelper

--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -1,6 +1,6 @@
 module GistsHelper
   def link_to_question(question)
-    link_to truncate(question.body), admin_question_path(question), target: '_blank'
+    link_to truncate(question.body, length: 25), admin_question_path(question), target: '_blank'
   end
 
   def hash_from_url(gist)
