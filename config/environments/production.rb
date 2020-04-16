c1601b043a93d6b880f7dad3152c3dc4336f88d8
ruslan <sqwer1986@gmail.com>
2020-04-16 18:31:30 +0300

    qq

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'thawing-spire-03896.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
+    address:              'smtp.mail.ru',
+    port:                 465,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
