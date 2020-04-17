122dd927b0ca49ff4d8ae8d79fbb88e5f6c45310
ruslan <sqwer1986@gmail.com>
2020-04-17 15:27:14 +0300

    fixs

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class FeedbacksMailer < ApplicationMailer
+  default to: -> { Admin.pluck(:email) }, subject: -> { t('.subject') }
+
   def send_feedback(feedback, user_email)
     @feedback = feedback
     @current_user_email = user_email
-    mail to: 'sqwer1986@gmail.com'
+    mail
   end
 end
