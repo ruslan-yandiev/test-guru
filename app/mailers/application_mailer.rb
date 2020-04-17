c8689f34a3d0ef75cb6e3d6cd038ed29f80e72fa
ruslan <sqwer1986@gmail.com>
2020-04-17 16:34:04 +0300

    fix 2

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: %("TestGuru <sqwer1986@gmail.com>")
+  default from: "sqwer1986@gmail.com"
   layout 'mailer'
 end
