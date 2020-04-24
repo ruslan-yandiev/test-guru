3db7e59b5f7b9ef65ec74ad49dd400da3b099f32
ruslan <sqwer1986@gmail.com>
2020-04-25 00:59:46 +0300

    add validations and associations

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,7 @@
 class Badge < ApplicationRecord
+  has_and_belongs_to_many :users
+
+  validates :name, presence: true, length: { in: 3..300 }
+  validates :url,  presence: true, length: { in: 3..300 }
+  validates :rule, presence: true, length: { in: 3..300 }
 end
