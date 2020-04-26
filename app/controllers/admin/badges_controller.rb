b92ace4dfdc55b3c38a47677ec2ffd6f8cac37df
ruslan <sqwer1986@gmail.com>
2020-04-27 01:56:00 +0300

    add method edit, update and destroy in admib badges_controller

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -2,8 +2,11 @@
 
 class Admin::BadgesController < Admin::BaseController
   before_action :set_badges, only: :index
+  before_action :set_badge, only: %i[show edit update destroy]
 
   def index; end
+
+  def show; end
 
   def new
   	@badge = Badge.new
@@ -19,6 +22,21 @@ class Admin::BadgesController < Admin::BaseController
     end
   end
 
+  def edit; end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badges_path(@badge)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
+
   private
 
   def set_badges
@@ -27,5 +45,9 @@ class Admin::BadgesController < Admin::BaseController
 
   def badge_params
   	params.require(:badge).permit(:name, :url, :rule)
+  end
+
+  def set_badge
+  	@badge = Badge.find(params[:id])
   end
 end
