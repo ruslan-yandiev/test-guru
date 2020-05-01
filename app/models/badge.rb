9d6e488a45afb22c95424074fa410e79b7b72cc4
ruslan <sqwer1986@gmail.com>
2020-05-01 12:17:33 +0300

    add new validates in badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -4,8 +4,8 @@ class Badge < ApplicationRecord
   has_many :badges_users, dependent: :destroy
   has_many :users, through: :badges_users
 
-  validates :name, presence: true, length: { in: 3..300 }
-  validates :url,  presence: true, length: { in: 3..300 }
-  validates :rule, presence: true, length: { in: 3..300 }
+  validates :name, :rule, presence: true, length: { in: 3..300 }
+  validates_uniqueness_of :rule, scope: :rule_value, case_sensitive: false
   validates :rule, inclusion: { in: Rule::ALL_BADGES_RULES, message: "%{value} is not a valid rule" }
+
 end
